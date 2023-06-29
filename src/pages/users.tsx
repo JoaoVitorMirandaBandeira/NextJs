c42a98ee6097a37ea833cde857cb303ca36db784
JoaoVitorMirandaBandeira <joaovitormirandabandeira@gmail.com>
2023-06-29 12:55:05 -0300

    iniciando o repositori

---
+++ src/pages/users.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const UsersPage = () => {
+  return (
+    <div>
+      <h1>Users Page</h1>
+      {/* Conteúdo da página */}
+    </div>
+  );
+};
+
+export default UsersPage;
